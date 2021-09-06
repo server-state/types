13ad4253b19b403c0e8e4282848f03472a6068ba
Pablo Klaschka <pklaschka@users.noreply.github.com>
2021-09-06 17:51:06 +0200

    feat: Update Extension API Types

--- src/extension-api.ts
+++ src/extension-api.ts
@@ -1,27 +1,61 @@
 import { Observable } from 'rxjs';
 import { JsonSerializable } from './json-serializable';
 
+export type PubSubHandleFunction = (...args: JsonSerializable[]) => void | JsonSerializable | Promise<JsonSerializable | void>;
+
 export interface ExtensionApi {
 	/**
-	 * Subscribes to a channel on the event bus.
-	 * @param channel - the channel to subscribe to
-	 * @returns an RXJS Observable
+	 * Calls a subroutine defined by the given channel name and returns a promise that resolves with its return value.
+	 *
+	 * Handled by {@link handle}
+	 * @param channel The channel name that defines the subroutine
+	 * @param args The args that get passed to {@link handle}
+	 * @returns {@link Promise} that resolves to the handler's `callback` return/resolve value
+	 * 
+	 * @see {@link handle}
 	 */
-	subscribe(channel: string): Observable<JsonSerializable>;
+	async request(channel: string, ...args: JsonSerializable[]): Promise<JsonSerializable>;
+
+	/**
+	 * Handles a request triggered by {@link request}.
+	 *
+	 * Only awaits "active subscription", not execution!
+	 * 
+	 * @param channel The channel to listen to. The channel defines the event.
+	 * @param callback The callback's return (/resolve) value gets passed back to {@link request}
+	 * @returns {@link Promise} that resolves to {@link Observable} once the subscription is active
+	 * 
+	 * @see {@link request}
+	 */
+	async handle(channel: string, callback: PubSubHandleFunction): Promise<void>
+
+	/**
+	 * Subscribes to a channel on the event bus.
+	 *
+	 * Only awaits "active subscription", not execution!
+	 * 
+	 * @param channel - the channel to subscribe to
+	 * @returns {@link Promise} that resolves to {@link Observable} once the subscription is active
+	 */
+	async subscribe(channel: string): Promise<Observable<JsonSerializable>>;
 
 	/**
 	 * Subscribes to channels on the event bus.
+	 *
+	 * Only awaits "active subscription", not execution!
+	 * 
 	 * @param channels - the channels to subscribe to
 	 * @returns an RXJS Observable which contains the message and the channel name
+	 * @returns {@link Promise} that resolves to {@link Observable} that emits `[message, channelName]` once the subscription is active
 	 */
-	subscribe(
+	async subscribe(
 		...channels: string[]
 	): Observable<readonly [message: JsonSerializable, channel: string]>;
 
 	/**
 	 * Subscribes to channels on the event bus using patterns.
 	 * @param patterns - the pattern(s) of channel(s) to subscribe to
-	 * @returns an RXJS Observable which contains the message and the channel name
+	 * @returns {@link Promise} that resolves to {@link Observable} that emits `[message, channelName]` once the subscription is active
 	 */
 	pSubscribe(
 		...patterns: string[]
@@ -31,7 +65,7 @@ export interface ExtensionApi {
 	 * Publishes a message to a channel on the event bus.
 	 * @param channel - the channel to publish to
 	 * @param message - the message to publish
-	 * @return an RXJS Observable which resolves when the message was successfully published
+	 * @return {@link Promise} which resolves when the message was successfully published
 	 */
 	publish(channel: string, message: JsonSerializable): Observable<void>;
 }
