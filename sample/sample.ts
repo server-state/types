30d198cc7c61a199be9c402ac4872db1b7670297
Pablo Klaschka <contact@pabloklaschka.de>
2021-09-20 13:45:42 +0200

    test(samples): :white_check_mark: Improve TypeScript samples (removing `// @ts-ignore` comment in the process)

--- sample/sample.ts
+++ sample/sample.ts
@@ -8,6 +8,8 @@ namespace AnotherPlace {
 	let jsonSerializable: ServerStateTypes.JsonSerializable;
 }
 
+const data: JsonSerializable = JSON.parse('"abc"');
+
 const api: ServerStateTypes.ExtensionApi = {
 	handle: (channel, callback) => {
 		return Promise.resolve();
@@ -18,11 +20,7 @@ const api: ServerStateTypes.ExtensionApi = {
 	publish: (channel, message) => {
 		return Promise.resolve();
 	},
-	// Necessary because of "TS-incompatible" types
-	// TODO: Research if there's any nicer way of doing this
-	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-	// @ts-ignore
-	subscribe: (channel, ...otherChannels) => {
+	subscribe(channel, ...otherChannels): Promise<Observable<any>> {
 		if (otherChannels.length === 0)
 			return Promise.resolve(new Observable<JsonSerializable>());
 		return Promise.resolve(new Observable<[JsonSerializable, string]>());
@@ -30,27 +28,28 @@ const api: ServerStateTypes.ExtensionApi = {
 	request: (channel, args) => Promise.resolve({ abc: 'abc' })
 };
 
-api
-	.subscribe('abc')
-	.then(value =>
-		value.subscribe({ next: json => `${json} is JsonSerializable` })
-	);
-
-api.subscribe('channel1', 'channel2').then(value =>
+void api.subscribe('abc').then(value =>
 	value.subscribe({
-		next: ([message, channel]) => `${message} on ${channel} is JsonSerializable`
+		next: json => `${JSON.stringify(json)} is JsonSerializable`
 	})
 );
 
-const data: JsonSerializable = JSON.parse('"abc"');
+void api.subscribe('channel1', 'channel2').then(value =>
+	value.subscribe({
+		next: ([message, channel]) =>
+			`${JSON.stringify(message)} on ${channel} is JsonSerializable`
+	})
+);
 
-api
+void api
 	.handle('REQUEST', async (arg1, arg2) => {
-		return Promise.resolve(`${arg1};${arg2}`);
+		return Promise.resolve(
+			`${(arg1 ?? 'null').toString()};${(arg2 ?? 'null').toString()}`
+		);
 	})
 	.then(() => {
 		// handler is now registered
-		api.request('REQUEST', 'a', 'b').then(res => {
+		void api.request('REQUEST', 'a', 'b').then(res => {
 			if (res !== 'a;b') throw new Error('Wrong');
 		});
 	});
